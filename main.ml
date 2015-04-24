a1121c8ede6247c9f58c2f55e5a04d960833f467
Khalid Tawil <tawilk2014@gmail.com>
2015-04-24 12:01:55 -0400

    debug io

--- main.ml
+++ main.ml
@@ -5,9 +5,23 @@ type 'a Vector = 'a array (* Might model after stacks *)
 type linProg = ('a Vector * matrix)
 
 type matrix = (int * int) * (elt array array)
-(array, newM)
 
-let io ( (n,p),m) : matrix ) : linProg =
-  let newM = empty x y in
-  match (get_row m 1) with
-  |(int, array) -> array in
+(* First parameter is our input, second is the current matrix.
+  To use this function, pass in an empty matrix as the second argument *)
+
+let rec io ( ((n,p),m) : matrix ) ( ((n1,p1),m1) : matrix) : linProg =
+  (
+    (match (get_row ((n,p),m) 1) with
+  | (_, array) -> array),
+  (removeTopRow ((n,p),m) (empty (n-1) p) 2)
+  )
+
+(* removeTopRow m (empty (n-1) p) 2 *)
+let rec removeTopRow ( (n,p),m) : matrix ) (newM : matrix) (counter : int) :
+  matrix =
+  if counter <= n then (
+    set_row newM counter (match (get_row ((n,p),m) counter) with
+                 | (_, a) -> a) ;
+    removeTopRow ((n,p),m) (counter+1) )
+  else
+    newM
