52f5da557a61149cf405226267b4ce0b0fcc5aec
Khalid Tawil <tawilk2014@gmail.com>
2015-04-24 13:54:27 -0400

    Organized main

--- main.ml
+++ main.ml
@@ -1,7 +1,7 @@
 open Core.Std
 open Matrix
 
-type 'a Vector = 'a array (* Might model after stacks *)
+type 'a Vector = 'a array
 type linProg = ('a Vector * matrix)
 
 type matrix = (int * int) * (elt array array)
@@ -9,14 +9,15 @@ type matrix = (int * int) * (elt array array)
 (* First parameter is our input, second is the current matrix.
   To use this function, pass in an empty matrix as the second argument *)
 
-let rec io ( ((n,p),m) : matrix ) ( ((n1,p1),m1) : matrix) : linProg =
+let rec io ( ((n,p),m) : matrix ) : linProg =
   (
     (match (get_row ((n,p),m) 1) with
-  | (_, array) -> array),
-  (removeTopRow ((n,p),m) (empty (n-1) p) 2)
+    | (_, array) -> array),
+    (removeTopRow ((n,p),m) (empty (n-1) p) 2)
   )
 
-(* removeTopRow m (empty (n-1) p) 2 *)
+(* Returns a matrix without the top row.
+  Ex: removeTopRow m (empty (n-1) p) 2 *)
 let rec removeTopRow ( (n,p),m) : matrix ) (newM : matrix) (counter : int) :
   matrix =
   if counter <= n then (
@@ -25,3 +26,23 @@ let rec removeTopRow ( (n,p),m) : matrix ) (newM : matrix) (counter : int) :
     removeTopRow ((n,p),m) (counter+1) )
   else
     newM
+
+(* prep : LinearProgram -> Cinput*)
+let prep (prog : linProg) =
+()
+
+(* simplex : Cinput -> Coutput*)
+let simplex =
+()
+
+(* unprep : Coutput -> float vector*)
+let unprep : float vector =
+()
+
+(* branch_and_bound : float vector -> LinearProgram*)
+let branch_and_bound (vec : float vector) : linProg =
+()
+
+(* geo_search : float vector -> LinearProgram -> int vector*)
+let geo_search (vec : float vector) (prog : linProg) : int vector =
+()
